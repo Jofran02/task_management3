18cdca2b13719e211f24b404e4b7e8ff52a7d5d8
Calvin Nugraha <calvinnugraha5@gmail.com>
2023-02-07 15:47:20 +0700

    eighth commit

--- lib/app/utils/widget/course.dart
+++ lib/app/utils/widget/course.dart
@@ -10,127 +10,137 @@ class course extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Column(
+    return Row(children: [
+      Column(children: [
+        Row(
           children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 30),
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlueAccent,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) {
-                            return TasksView();
-                          },
-                        ));
-                      },
-                      child: Column(
-                        children: [
-                          Text(""),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 30,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  height: 150,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.redAccent,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) {
-                          return TasksView();
-                        },
-                      ));
-                    },
-                    child: Column(
-                      children: [
-                        const Text(""),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
             Padding(
               padding: const EdgeInsets.only(left: 30),
-              child: Row(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.purpleAccent,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) {
-                            return TasksView();
-                          },
-                        ));
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return TasksView();
                       },
-                      child: Column(
-                        children: [
-                          const Text(""),
-                        ],
+                    ));
+                  },
+                  child: Column(
+                    children: const [
+                      Text(
+                        "Pemrograman Aplikasi Mobile",
+                        style: TextStyle(color: Colors.white),
                       ),
-                    ),
+                    ],
                   ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  Container(
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlue,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) {
-                            return TasksView();
-                          },
-                        ));
-                      },
-                      child: Column(
-                        children: [
-                          const Text(""),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 30,
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              height: 150,
+              width: 150,
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return TasksView();
+                    },
+                  ));
+                },
+                child: Column(
+                  children: const [
+                    Text("Metode Numerik",
+                        style: TextStyle(color: Colors.white)),
+                  ],
+                ),
               ),
             ),
           ],
         ),
-      ],
-    );
+        const SizedBox(
+          height: 30,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 30),
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.purpleAccent,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return TasksView();
+                      },
+                    ));
+                  },
+                  child: Column(
+                    children: const [
+                      Text("UI & UX Design",
+                          style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 5),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) {
+                              return TasksView();
+                            },
+                          ));
+                        },
+                        child: Column(
+                          children: const [
+                            Text("Video Editing",
+                                style: TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+      ]),
+    ]);
   }
 }
